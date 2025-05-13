45a985b4d69044cae0a8772f62f66a139393f2fc
Daniel Ramalho <danieltjramalho@gmail.com>
2025-05-13 19:02:33 +0100

    cubeparticles

--- Projeto_Frontend/canvas.js
+++ Projeto_Frontend/canvas.js
@@ -1,4 +1,5 @@
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
+//import animatecube from './Animate-requests.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,12 +10,6 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setClearColor(0x000000, 0);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-
-
-/* Responsive canvas */
-
-let initialWidth = window.innerWidth;
-let initialHeight = window.innerHeight;
 
 //reloads quando windowmode
 window.addEventListener('resize', () => {
@@ -34,72 +29,83 @@ window.addEventListener('resize', () => {
   });
 
 /* CUBE MESH */
-
-let cube;
-let cube1;
-let cube2;
-let cube3;
-let cube4;
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { transparent: true, emissive: 0x00ffcc, emissiveIntensity: 1.5, color: 0xFF9100 } );
-cube = new THREE.Mesh( geometry, material );
-cube1 = new THREE.Mesh( geometry, material );
-cube2 = new THREE.Mesh( geometry, material );
-cube3 = new THREE.Mesh( geometry, material );
-cube4 = new THREE.Mesh( geometry, material );
-function createcube (){
-
-
-}
-
 
 
 camera.position.z = 5;
-
 /* CUBE RANDOM GENERATION */
 
-let xposition; /*define position em que o cube nasce*/
-let zposition; /*define position em que o cube nasce*/
-let cubesize = 1; /*define tamanho do cube*/
-
-let randomnum = () => {let randompos = Math.random() * (15 - (-15)) + (-15); xposition = randompos; zposition = randompos};
-let randomsize = () => {let randomsz = Math.floor(Math.random() * 5) - 1; cubesize = randomsz};
+function randomposi(){
+    return Math.random() * 40 - 20;
+}
+function randomsize(){
+    return Math.random() * (4.0 - 0.1) + 0.1;
+}
 function rng(){
     randomnum();
     randomsize();
-    console.log(xposition);
-    scene.add(cube);
-    animate();
-    randomnum();
 }
+
+
+/*INSTANCES-OF-CUBE*/
+let maxstorage = 9;
+let arrayxpos = new Array(maxstorage);
+let arrasize = new Array(maxstorage);
+
+function randomnum(){
+    for(let i = 0; i<9; i++){
+        arrayxpos[i] = randomposi();
+        console.log(arrayxpos);
+        
+        arrasize[i] = randomsize();
+        console.log(arrayxpos);
+    }
+}
+
+let cubesstorage = [];
+function cubegererator(){
+    for(let i = 0; i<maxstorage; i++){
+        let newmaterial = new THREE.MeshBasicMaterial({ transparent: true, color: 0xFF9100 });
+        let cubes= new THREE.Mesh(geometry, newmaterial);
+
+        scene.add(cubes);
+        cubes.position.z = randomposi();
+        cubes.material.opacity = 1;
+        cubes.position.y = randomposi();
+        cubesstorage.push(cubes);
+    }
+    animateAllCubes();
+}
+cubegererator();
+
 rng();
 
-/*CUBE LIFE-CYCLE*/
-
-function animate() {
-    requestAnimationFrame(animate);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-    cube.position.x = xposition;
-    cube.position.y += 0.02;
-    cube.scale.set(cubesize, cubesize, cubesize); /*tamanho do cubo*/
-	console.log(cubesize);
+function animatecubes(cubecopy, index) {
+    cubecopy.position.z = -13; 
+	cubecopy.rotation.x += 0.01;
+	cubecopy.rotation.y += 0.01;
+    cubecopy.position.x = arrayxpos[index];
+    cubecopy.position.y += 0.02;
+    cubecopy.scale.set(arrasize[index], arrasize[index], arrasize[index]); /*tamanho do cubo*/
     renderer.render( scene, camera );
-    cube.position.z = -13; 
 
-    if (cube.position.y < 6) {
-        cube.material.opacity = Math.min(cube.material.opacity + 0.01, 1);
+    if (cubecopy.position.y < 6) {
+        cubecopy.material.opacity = Math.min(cubecopy.material.opacity + 0.01, 1);
     } else {
-        cube.material.opacity = Math.max(cube.material.opacity - 0.01, 0);
+        cubecopy.material.opacity = Math.max(cubecopy.material.opacity - 0.01, 0);
     }
 
-if(cube.position.y > 13) {
-   cube.position.y = -13; 
-   randomsize();
-   randomnum();
+if(cubecopy.position.y > 13) {
+   cubecopy.position.y = -12; 
+   arrasize[index] = randomsize();
+   arrayxpos[index] = randomposi();
 }
 }
 
-
-
-
+function animateAllCubes() {
+    requestAnimationFrame(animateAllCubes);
+    for (let i = 0; i < cubesstorage.length; i++) {
+        animatecubes(cubesstorage[i], i);
+    }
+    renderer.render(scene, camera);
+}
