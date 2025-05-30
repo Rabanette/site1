9fe9e393615aee1d569d65bd3d6bf70155075f2c
Daniel Ramalho <danieltjramalho@gmail.com>
2025-05-30 10:37:22 +0100

    css

---
+++ Projeto_Frontend/Computer.js
@@ -0,0 +1,1 @@
+let canva = document.getElementById('PCcanvas');
---
+++ Projeto_Frontend/data.js
@@ -0,0 +1,9 @@
+export default PortfolioLib[
+
+    {
+        'title': Portfolio,
+        'Text': lorem,
+        'ImageURL': Portfolio.png,
+    }
+
+]
