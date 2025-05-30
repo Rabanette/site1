ef226ea70d75071bc734b8cd36e1a8eee57383ad
Daniel Ramalho <danieltjramalho@gmail.com>
2025-05-30 10:44:00 +0100

    css

--- Projeto_Frontend/data.js
+++ Projeto_Frontend/data.js
@@ -4,6 +4,17 @@ export default PortfolioLib[
         'title': Portfolio,
         'Text': lorem,
         'ImageURL': Portfolio.png,
+    },
+    
+    {
+        'title': Before_The_Storm,
+        'Text': lorem,
+        'ImageURL': Game.png,
+    },
+    
+    {
+        'title': PAP,
+        'Text': lorem,
+        'ImageURL': PAPsite.png,
     }
-
 ]
