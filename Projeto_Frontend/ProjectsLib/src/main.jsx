1e193dc64d2e71724c7d92527613161951cf7dae
Daniel Ramalho <danieltjramalho@gmail.com>
2025-07-16 22:14:25 +0100

    update

--- Projeto_Frontend/ProjectsLib/src/main.jsx
+++ Projeto_Frontend/ProjectsLib/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import ProjectsSection from './Components/ProjectsSection.jsx'
 import './index.css'
-import '../RestonsiveCSSs/stylesPhone.css'
-import '../RestonsiveCSSs/stylesTablet.css'
-import '../RestonsiveCSSs/stylesPC.css'
+import './RestonsiveCSSs/stylesPhone.css'
+import './RestonsiveCSSs/stylesTablet.css'
+import './RestonsiveCSSs/stylesPC.css'
 
 createRoot(document.getElementById('root')).render(
 
