0a5b1a1856f48aa3f413c931ad4f06e851fb1eb0
Daniel Ramalho <danieltjramalho@gmail.com>
2025-07-28 11:26:46 +0100

    react

--- Projeto_Frontend/ProjectsLib/vite.config.js
+++ Projeto_Frontend/ProjectsLib/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'https://rabanette.github.io/site1/',
-  plugins: [react(),],
+  base:'/site1/',
+  plugins: [react(),  deploy()],
 })
